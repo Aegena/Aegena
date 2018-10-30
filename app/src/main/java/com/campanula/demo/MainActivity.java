6e26c3bfe0754ac0c5737c5eab21c383ccb1be4c
wd910617 <1104107956@qq.com>
2018-10-30 18:52:59 +0800

    初始化项目

---
+++ app/src/main/java/com/campanula/demo/MainActivity.java
@@ -0,0 +1,100 @@
+package com.campanula.demo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.campanula.base.BaseActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author campanula
+ * create 2018-10-30
+ * @since
+ */
+public class MainActivity extends BaseActivity {
+
+    RecyclerView mRecyclerView;
+    ItemsAdapter mItemsAdapter;
+
+    @NotNull
+    @Override
+    protected String getPackagePath() {
+        return MainActivity.class.getSimpleName();
+    }
+
+    @Override
+    protected void bindViewData() {
+        mItemsAdapter = new ItemsAdapter();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        mRecyclerView.setAdapter(mItemsAdapter);
+
+    }
+
+    @Override
+    protected void findActivityViewById() {
+        mRecyclerView = findViewById(R.id.mRecyclerView);
+    }
+
+    @Override
+    protected int getLayoutViewId() {
+        return R.layout.main_activity;
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView mImageView;
+        TextView mTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.mImageView);
+            mTextView = itemView.findViewById(R.id.mTextView);
+        }
+    }
+
+    static class ItemsAdapter extends RecyclerView.Adapter<ViewHolder> {
+        String[] args = new String[]{
+                "",
+                ""
+        };
+        String[] names = new String[]{
+                "QQ红点",
+                "水波纹"
+        };
+        int[] icons = new int[]{
+                R.mipmap.ic_launcher
+        };
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int itemType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main_activity, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            viewHolder.mTextView.setText(names[i]);
+            viewHolder.mImageView.setImageResource(icons[0]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return args.length;
+        }
+    }
+
+
+}
